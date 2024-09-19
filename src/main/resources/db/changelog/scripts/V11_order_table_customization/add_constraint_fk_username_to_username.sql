ad60a187e4641ae2623fa0dcfcdc3a73567f3535
NoWaaLT <vytautas.jelisejevas@gmail.com>
2024-09-19 11:28:48 +0300

    - Transactions added; - One part of cache created; - MapStruct mapping applied; - Separated logic between layers; - Http responses set up;

--- src/main/resources/db/changelog/scripts/V11_order_table_customization/add_constraint_fk_username_to_username.sql
+++ src/main/resources/db/changelog/scripts/V11_order_table_customization/add_constraint_fk_username_to_username.sql
@@ -1,9 +1,1 @@
-ALTER TABLE orders DROP CONSTRAINT fk_orders_user;
-
-ALTER TABLE orders ADD CONSTRAINT fk_orders_user FOREIGN KEY (fk_username) REFERENCES users(username);
-
---ALTER TABLE orders DROP FOREIGN KEY fk_orders_user;
---
---ALTER TABLE orders
---ADD CONSTRAINT fk_orders_user
---FOREIGN KEY (fk_username) REFERENCES users(username);
+ALTER TABLE orders ADD CONSTRAINT fk_orders_username FOREIGN KEY (fk_username) REFERENCES users(username);
